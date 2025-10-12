b192955c6dd51fafdbcd045d5d6c6f2c81db6c15
Abel COIFFARD <coiffard.abel@gmail.com>
2025-10-12 23:56:12 +0200

    Finished Exo2 4mn before deadline...  I hope?

--- Week-5/Exercises/blindspot.py
+++ Week-5/Exercises/blindspot.py
@@ -1,10 +1,8 @@
 from expyriment import design, control, stimuli
 from expyriment.misc.constants import C_WHITE, C_BLACK
 
-""" Global settings """
-exp = design.Experiment(name="Blindspot", background_colour=C_WHITE, foreground_colour=C_BLACK)
-control.set_develop_mode()
-control.initialize(exp)
+data = ('', 0, 0, (0, 0))
+full_data = []
 
 """ Stimuli """
 def make_circle(r, pos=(0,0)):
@@ -13,9 +11,9 @@ def make_circle(r, pos=(0,0)):
     return c
 
 """Instructions"""
-def display_instr():
-    text = stimuli.TextScreen(heading = "Blindspot Finder", text = "Cover your right eye\n\n " \
-    "Fix the cross with your left eye\n\n " \
+def display_instr(side):              
+    text = stimuli.TextScreen(heading = "Blindspot Finder", text = f"Cover your {side} eye\n\n " \
+    "Fix the cross with your other eye\n\n " \
     "Move the circle with your keyboard's arrows until it disappears\n\n " \
     "The circle should disappear when entering your blindspot. If it doesn't, try reducing its size by pressing 2\n\n " \
     "Then try increasing the circle size by pressing 1 and see how big it can be while the effect still working\n\n" \
@@ -23,14 +21,28 @@ def display_instr():
     text.present(clear = True, update = True)
     exp.keyboard.wait()
 
-""" Experiment """
-def run_trial(radius = 75, posCircleX = 0, posCircleY = 0):
-    key = 0
-    display_instr()
-    while(not(key == 32)):
-        fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[300, 0])
-        fixation.preload()
+""" Input """
+def input_side(prompt):
+    """ Asks user for a R or L input."""
+    ans = input(prompt) # Ask the user for their guess
+    while True: # Repeat until the user inputs a valid letter
+        if ans=='R':
+            return 1
+        if ans=='L':
+            return -1
+        print('Please, enter "L" or "R"')
+        ans = input(prompt)  
 
+""" Experiment """
+def run_trial(radius = 75, posCircleX = 0, posCircleY = 0, side = 1, rec_full = False):
+    """ Global settings """
+    key = 0
+    fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[side*300, 0])
+    fixation.preload()
+    if side + 1: side='right' 
+    else: side='left'
+    display_instr(side)
+    while(not(key == 32)):
         circle = make_circle(radius, (posCircleX, posCircleY))
 
         fixation.present(True, False)
@@ -50,10 +62,26 @@ def run_trial(radius = 75, posCircleX = 0, posCircleY = 0):
             posCircleY += 5
         elif(key == 1073741905):
             posCircleY -= 5
+        
+        if rec_full:
+            data = (side, key, radius, (posCircleX, posCircleY))
+            full_data.append(data)
+            
+    data = (side, key, radius, (posCircleX, posCircleY))
 
 
+
+side = input_side("Please enter 'R' to test the right eye or 'L' for the left one: ")
+exp = design.Experiment(name="Blindspot", background_colour=C_WHITE, foreground_colour=C_BLACK)
+control.set_develop_mode()
+control.initialize(exp)
 control.start(subject_id=1)
-
-run_trial()
+run_trial(side=side, rec_full = True)
     
 control.end()
+
+if(len(full_data)):
+   for side, key, radius, pos in full_data:
+    print(f"Trial on {side} eye: key {key} made circle of radius {radius} at pos ({pos[0]}, {pos[1]})")
+else:
+    print(f"Trial on {data[0]} eye: key {data[1]} made circle of radius {data[2]} at pos ({data[3][0]}, {data[3][1]})")
